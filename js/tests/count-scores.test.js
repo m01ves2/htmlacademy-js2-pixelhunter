c1a115594ca342c8c045ae508e576e81d4b18539
Eugeny Vlasov <eugenyvlasov@gmail.com>
2024-03-07 01:33:51 +0300

    count-scores.test.js updated to new countScores() method testing

--- js/tests/count-scores.test.js
+++ js/tests/count-scores.test.js
@@ -1,71 +1,62 @@
-// import { assert } from 'chai';
-// import { ANSWER_TIME_LIMIT, countScores } from '../utils/count-scores';
+import { assert } from 'chai';
+import { countScores } from '../utils/count-scores';
+import { initialState } from '../data/game-data';
 
-// const generateMockData = function (numberOfElements, numberOfValid, numberOfSlowAnswers, numberOfFastAnswers) {
-//   let answersArray = [];
-//   let numberOfValidLeft = numberOfValid;
-//   let numberOfSlowAnswersLeft = numberOfSlowAnswers;
-//   let numberOfFastAnswersLeft = numberOfFastAnswers;
+//RESULT_STATE = [ `wrong`, `correct`, `slow`, `fast`, `unknown`, ]
+const generateMockData = function (answersCounter, correctCounter, slowCounter, fastCounter, wrongCounter) {
+  let answersArray = [];
+  let unknownCounter = answersCounter - (correctCounter + slowCounter + fastCounter + wrongCounter);
 
-//   for (let i = 0; i < numberOfElements; i++) {
-//     let el = {};
-//     el.isValid = numberOfValidLeft-- > 0 ? true : false;
+  let state = Object.assign({}, initialState, {answers: answersArray});
+  if(wrongCounter > 3){
+    wrongCounter = 3;
+  }
+  state.lives -= wrongCounter;
 
-//     //TODO think about DRY here. make more oprimized generating method
-//     if (Math.round(Math.random()) == 0) {
-//       if (numberOfFastAnswersLeft-- > 0) {
-//         el.time = Math.floor(Math.random() * ANSWER_TIME_LIMIT.FAST);
-//       }
-//       else if (numberOfSlowAnswersLeft-- > 0) {
-//         el.time = Math.floor(ANSWER_TIME_LIMIT.SLOW + Math.random() * 100);
-//       }
-//       else {
-//         el.time = Math.floor(ANSWER_TIME_LIMIT.FAST + Math.random() * (ANSWER_TIME_LIMIT.SLOW - ANSWER_TIME_LIMIT.FAST));
-//       }
-//     }
-//     else {
-//       if (numberOfSlowAnswersLeft-- > 0) {
-//         el.time = Math.floor(ANSWER_TIME_LIMIT.SLOW + Math.random() * 100);
-//       }
-//       else if (numberOfFastAnswersLeft-- > 0) {
-//         el.time = Math.floor(Math.random() * ANSWER_TIME_LIMIT.FAST);
-//       }
-//       else {
-//         el.time = Math.floor(ANSWER_TIME_LIMIT.FAST + Math.random() * (ANSWER_TIME_LIMIT.SLOW - ANSWER_TIME_LIMIT.FAST));
-//       }
-//     }
-//     answersArray.push(el)
-//   };
+  if( unknownCounter < 0 ){
+    return -1;
+  }
 
-//   return answersArray;
-// }
+  for(let  i = 0; i < correctCounter; i++){
+    answersArray.push(`correct`);
+  }
+
+  for(let  i = 0; i < slowCounter; i++){
+    answersArray.push(`slow`);
+  }
+
+  for(let  i = 0; i < fastCounter; i++){
+    answersArray.push(`fast`);
+  }
+
+  for(let  i = 0; i < wrongCounter; i++){
+    answersArray.push(`wrong`);
+  }
+
+  for(let  i = 0; i < unknownCounter; i++){
+    answersArray.push(`unknown`);
+  }
+
+  return state;
+}
 
 
-// describe(`countScores()  function tests`, () => {
-//   describe(`incorrect or invalid parameters`, () => {
-//     it(`should return -1 when the answersArray has less then 10 elements`, () => {
-//       let answersArray = generateMockData(3, 2, 1, 1);
-//       let livesLeftCounter = 3;
-//       assert.equal(-1, countScores(answersArray, livesLeftCounter));
-//     });
-//     it(`should return -1 if livesLeftCounter is invalid`, () => {
-//       let answersArray = generateMockData(10, 0, 5, 5);
-//       assert.equal(-1, countScores(answersArray, null));
-//       assert.equal(-1, countScores(answersArray, undefined));
-//       assert.equal(-1, countScores(answersArray, `dfsgds`));
-//     });
-//   });
+describe(`countScores()  function tests`, () => {
+  describe(`incorrect or invalid parameters`, () => {
+    it(`should return -1 when the answersArray has less then 10 elements`, () => {
+      let state = generateMockData(9, 2, 1, 1, 1);
+      assert.equal(-1, countScores(state));
+    });
+  });
 
-//   describe(`valid parameters, checking results`, () => {
-//     it(`should return 1150 pts if 10 answers are valid and livesLeftCounter = 3`, () => {
-//       let answersArray = generateMockData(10, 10, 5, 5);
-//       let livesLeftCounter = 3;
-//       assert.equal(1150, countScores(answersArray, livesLeftCounter));
-//     });
-//     it(`should return 0 pts if 0 answers are valid and livesLeftCounter = 0`, () => {
-//       let answersArray = generateMockData(10, 0, 5, 5);
-//       let livesLeftCounter = 0;
-//       assert.equal(0, countScores(answersArray, livesLeftCounter));
-//     });
-//   });
-// });
+  describe(`valid parameters, checking results`, () => {
+    it(`should return 1150 pts if 10 answers are valid and livesLeftCounter = 3`, () => {
+      let state = generateMockData(10, 10, 0, 0, 0);
+      assert.equal(1150, countScores(state));
+    });
+    it(`should return 0 pts if 0 answers are valid and livesLeftCounter = 0`, () => {
+      let state = generateMockData(10, 0, 0, 0, 3);
+      assert.equal(0, countScores(state));
+    });
+  });
+});
